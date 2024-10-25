04df1cd238331aa3619c53a056b6cb366c4b0233
ogithaka <ogithaka@gmail.com>
2024-10-25 17:49:21 +0300

    feat: enhance footer and navigation elements
    - Added copyright notice to the footer for ownership clarity and professionalism.
    - Applied a subtle blur effect to the header and navigation dropdown to improve visibility and modernize the design.

--- app/components/layouts/Footer.tsx
+++ app/components/layouts/Footer.tsx
@@ -1,5 +1,9 @@
 import { FooterStyles } from '@layoutsStyles';
 
 export default function Footer() {
-    return <footer className={FooterStyles.Footer}></footer>;
+    return (
+        <footer className={FooterStyles.Footer}>
+            <p>© {new Date().getFullYear()} Guardy. All rights reserved.</p>
+        </footer>
+    );
 }
