a60d839dd5c393aaad2209741564254f22b1741a
ogithaka <ogithaka@gmail.com>
2024-10-25 15:40:54 +0300

    style: centralize Logo component styling for maintainability
    - Introduced `Logo.module.css` in `styles/shared` to encapsulate Logo-specific styles, ensuring consistency and easier maintenance.
    - Added `shared.ts` in `exports/styles` to act as a central export point, enhancing style reusability.
    - Updated `tsconfig.json` with `@sharedStyles` path alias for simplified imports of shared styles.
    
    This centralization fosters code organization, maintainability, and prepares the app for future theming and customization.

---
+++ app/components/shared/Brand.tsx
@@ -0,0 +1,13 @@
+import { Logo } from '@sharedComponents';
+
+export default function Brand() {
+    return (
+        <div>
+            <Logo
+                width={35}
+                height={35}
+            />
+            <p>Guardy</p>
+        </div>
+    );
+}
