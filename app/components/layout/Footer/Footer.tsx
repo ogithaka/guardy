27b8aa5e59ccf9c3bf99aa04190bcbb06f593d3d
ogithaka <ogithaka@gmail.com>
2024-10-25 09:44:11 +0300

    chore: reset

--- app/components/layout/Footer/Footer.tsx
+++
@@ -1,9 +0,0 @@
-import { FooterStyles } from '@components';
-
-export default function Footer() {
-    return (
-        <footer className={FooterStyles.Footer}>
-            <p>&copy; 2024 Guardy. All rights reserved.</p>
-        </footer>
-    );
-}
--- app/components/widgets/Form/Form.tsx
+++
@@ -1,5 +0,0 @@
-import { FormStyles, Logo } from '@components';
-
-export default function Form({ children }: FormProps) {
-    return <form className={FormStyles.Form}>{children}</form>;
-}
